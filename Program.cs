ca846e788cfa4a3c12740583c5095f80b5fafcb9
Lívia Raiane <liviaraianen@gmail.com>
2023-12-03 19:48:54 -0300

    feat: Criando herança e instâncias

--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("--------------- Iphone ---------------\n");
+Smartphone iphoneX = new Iphone(numero: "123456789", modelo: "iphone x", imei: "354224789123456", memoria: 64);
+iphoneX.Ligar();
+iphoneX.ReceberLigacao();
+iphoneX.InstalarAplicativo("Instagram");
+
+Console.WriteLine("\n\n--------------- Nokia ---------------\n");
+Smartphone lumia = new Nokia(numero: "987654321", modelo: "Nokia Lumia 520", imei: "865309876543210", memoria: 8);
+lumia.Ligar();
+lumia.ReceberLigacao();
+lumia.InstalarAplicativo("WhatsApp");
